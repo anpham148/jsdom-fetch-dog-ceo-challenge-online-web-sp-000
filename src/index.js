8e426f594d76c1e09e0693ffeccaf35efc07fc7f
anpham148 <anpham148@gmail.com>
2021-02-02 16:15:55 +0000

    commit tree 4fb3172b2357afd20af62c636758939c8daa6730

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", () => {
+    fetchImages();
+
+});
+
+
+function fetchImages() {
+    fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json())
+    .then(json => json['message'].forEach(image => renderImages(image)))
+  }
+
+  function renderImages(image) {
+     const image_container = document.querySelector('.dog-image-container')
+     let img = document.createElement('img');
+     img.src = image;
+     image_container.appendChild(img);
+  }
+
+
+  
